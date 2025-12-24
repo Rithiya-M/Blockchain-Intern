a78143108eae54668b113b60d1806ae25c3bbb3c
Rithiya-M <rithiyarithu28@gmail.com>
2025-12-24 10:56:29 +0530

    Add files via upload

---
+++ intern.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/access/AccessControl.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.0/contracts/token/ERC20/ERC20.sol";
+contract MyToken is ERC20, AccessControl {
+bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
+constructor() public ERC20("MyToken", "TKN") {
+_setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+}
+function mint(address to, uint256 amount) public {
+require(hasRole(MINTER_ROLE, msg.sender), "Caller is not a minter");
+_mint(to, amount);
+}
+function burn(address from, uint256 amount) public {
+require(hasRole(BURNER_ROLE, msg.sender), "Caller is not a burner");
+_burn(from,amount);
+}
+}
---
+++ task1.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SimpleToken {
+    string public name = "SimpleToken";
+    string public symbol = "STK";
+    uint8 public decimals = 18;
+    uint public totalSupply;
+
+    mapping(address => uint) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint value);
+
+    constructor(uint _initialSupply) {
+        totalSupply = _initialSupply * (10 ** decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool) {
+        require(balanceOf[msg.sender] >= _value, "Insufficient balance");
+        require(_to != address(0), "Invalid address");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+}
